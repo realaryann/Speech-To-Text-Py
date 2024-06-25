03ed4b202ca9d1d882553b5576d5830353baa7b5
aryan <arcnagpal1234@gmail.com>
2024-06-25 16:42:57 -0400

    fast version

--- inputaudio.py
+++ inputaudio.py
@@ -9,17 +9,7 @@ import numpy
 from scipy.io.wavfile import write
 
 def write_audio(recording, freq_aud) -> None:
-	with open("./log/log_rec.txt", "r+") as reclogs:
-		reclines = reclogs.readlines()
-		if len(reclines) == 0:
-			filename = f"recording0.wav"
-			reclogs.write("0\n")
-		else:
-			num = int(reclines[-1][0:-1:]) + 1
-			filename = f"recording{num}.wav"
-			reclogs.write(str(num)+'\n')
-
-	storage = f"./recordings/{filename}"
+	storage = f"./recordings/recording.wav"
 	write(storage, freq_aud, recording)
 
 def record_audio(freq_aud: int, duration: int):
