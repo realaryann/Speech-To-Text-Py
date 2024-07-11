16db97299ae9894b0bfa67b5f09612ab082f2682
aryan <arcnagpal1234@gmail.com>
2024-07-11 10:08:38 -0400

    Loop till finished

--- input.sh
+++ input.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-echo "INFO: Running audio listener"
+echo "INFO: Running audio listener and transcriber"
 python3 inputaudio.py
-echo "INFO: Audio listener finished"
-echo "INFO: Running audio transcriber"
-python3 voskhear.py
-echo "INFO: Audio transcriber finished"
 echo " "
 cat ./results/test.txt
--- inputaudio.py
+++ inputaudio.py
@@ -4,10 +4,11 @@ arg1 = duration of recording
 '''
 import sys
 import sounddevice as sd
-import find_device as fd
+import find_device as fd 
 import numpy
 import keyboard
 from scipy.io.wavfile import write
+import voskcall as v
 
 def write_audio(recording, freq_aud) -> None:
 	storage = f"./recordings/recording.wav"
@@ -26,33 +27,52 @@ def read_keyboard():
 			return 10
 		elif keyboard.is_pressed('y'):
 			return 20
+		elif keyboard.is_pressed('z'):
+			exit()
 
 def main():
 	# Frequency of audio
 	freq_aud = 44100
 	duration = 0
-	try:
+	while True:
 		print("R: 5 Seconds")
 		print("T: 10 Seconds")
 		print("Y: 20 Seconds")
+		print("Z: Exit Program")
 		print("\nINFO: Reading from the keyboard now")
 		duration = read_keyboard()
-	except:
-		print("\nError recording keyboard input")
-	print("\nINFO: Finished reading keyboard input")
-	try:
-		sd.default.device = fd.find_device()
-	except Exception as e:
-		print(f"Finding {fd.DEVICE_NAME} failed: {e}\n")
-	try:
-		print(f"INFO: Recording now, for {duration} seconds")
-		recording = record_audio(freq_aud, duration)
-	except Exception as e:
-		print(f"Recording audio failed: {e}\n")
-	try:
-		write_audio(recording, freq_aud)
-	except Exception as e:
-		print(f"Writing audio failed: {e}\n")
+
+		try:
+			sd.default.device = fd.find_device()
+		except Exception as e:
+			print(f"Finding {fd.DEVICE_NAME} failed: {e}\n")
+
+		try:
+			print(f"INFO: Recording now, for {duration} seconds")
+			recording = record_audio(freq_aud, duration)
+		except Exception as e:
+			print(f"Recording audio failed: {e}\n")
+
+		try:
+			write_audio(recording, freq_aud)
+		except Exception as e:
+			print(f"Writing audio failed: {e}\n")
+
+		try:
+			filename = "recordings/recording.wav"
+			model_path = "/home/csrobot/vosktest/models/vosk-model-en-us-0.22"
+
+			transcriber = v.Transcriber(model_path)
+			transcription = transcriber.transcribe(filename)
+
+			newf = "test.txt"
+
+			filesrc = f"./results/{newf}"
+			with open(filesrc, "w") as tf:
+				for i in transcription:
+					tf.write(i+'\n')
+		except Exception as e:
+			print("ERROR: Error while transcribing the recording")
 
 if __name__ == "__main__":
 	main()
